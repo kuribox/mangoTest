b9b6f236a9c12a76048a846d998a317d9ebb6383
Martin Baratto <mr.baratto@unlix.net>
2023-02-02 12:06:37 +0100

    Home menu

--- src/views/Exercice2/index.jsx
+++ src/views/Exercice2/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Exercice2 = () => {
   return (
     <div>
-      Exercice1
+      Exercice2
     </div>
   );
 };
--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "./home.module.css";
 
 const Home = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      Home
+    <div className={styles.container}>
+      <div className={styles.button} onClick={() => navigate("/exercise1")}>
+        Exercise 1
+      </div>
+      <div className={styles.button} onClick={() => navigate("/exercise2")}>
+        Exercise 2
+      </div>
     </div>
   );
 };
